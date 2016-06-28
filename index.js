54c4ce11fbbd7ea538c0a170413d7422ea2f46d4
Macbook Air <michael.hueber@gmail.com>
2016-06-28 23:27:31 +0200

    Init

---
+++ index.js
@@ -0,0 +1,146 @@
+// MQTT Switch Accessory plugin for HomeBridge
+//
+// Remember to add accessory to config.json. Example:
+// "accessories": [
+//     {
+//            "accessory": "mqttswitch",
+//            "name": "PUT THE NAME OF YOUR SWITCH HERE",
+//            "url": "PUT URL OF THE BROKER HERE",
+//			  "username": "PUT USERNAME OF THE BROKER HERE",
+//            "password": "PUT PASSWORD OF THE BROKER HERE"
+// 			  "caption": "PUT THE LABEL OF YOUR SWITCH HERE",
+// 			  "topics": {
+// 				"statusGet": 	"PUT THE MQTT TOPIC FOR THE GETTING THE STATUS OF YOUR SWITCH HERE",
+// 				"statusSet": 	"PUT THE MQTT TOPIC FOR THE SETTING THE STATUS OF YOUR SWITCH HERE"
+// 			  }
+//     }
+// ],
+//
+// When you attempt to add a device, it will ask for a "PIN code".
+// The default code for all HomeBridge accessories is 031-45-154.
+
+'use strict';
+
+var Service, Characteristic;
+var mqtt = require("mqtt");
+var temperatureService;
+var humidityService;
+var switchService;
+
+module.exports = function(homebridge) {
+  	Service = homebridge.hap.Service;
+  	Characteristic = homebridge.hap.Characteristic;
+
+  	homebridge.registerAccessory("homebridge-mqttswitch", "mqttswitch", MqttSwitchAccessory);
+}
+
+
+function MqttSwitchAccessory(log, config) {
+  	this.log          	= log;
+	this.model			= config["model"];
+	this.serial			= config["serial"];
+	this.manufacturer	= config["manufacturer"];
+  	this.name 			= config["name"];
+  	this.url 			= config["url"];
+	this.client_Id 		= 'mqttjs_' + Math.random().toString(16).substr(2, 8);
+	this.options = {
+	    keepalive: 10,
+    	clientId: this.client_Id,
+	    protocolId: 'MQTT',
+    	protocolVersion: 4,
+    	clean: true,
+    	reconnectPeriod: 1000,
+    	connectTimeout: 30 * 1000,
+		will: {
+			topic: 'WillMsg',
+			payload: 'Connection Closed abnormally..!',
+			qos: 0,
+			retain: false
+		},
+	    username: config["username"],
+	    password: config["password"],
+    	rejectUnauthorized: false
+	};
+	this.caption		= config["caption"];
+	this.topicStatusGet	= config["topics"].statusGet;
+	this.topicStatusSet	= config["topics"].statusSet;
+
+    this.topicTemperature	= config["topics"].temperature;
+    this.topicHumidity = config["topics"].humidity;
+
+	this.switchStatus = false;
+    this.temperature = 0.0;
+    this.humidity = 0.0;
+
+	// connect to MQTT broker
+	this.client = mqtt.connect(this.url, this.options);
+	var that = this;
+	this.client.on('error', function () {
+		that.log('Error event on MQTT');
+	});
+
+	this.client.on('message', function (topic, message) {
+		if (topic == that.topicStatusGet) {
+			var status = message.toString();
+			that.switchStatus = (status == "true" ? true : false);
+		   	switchService.getCharacteristic(Characteristic.On).setValue(that.switchStatus, undefined, 'fromSetValue');
+		}
+        if (topic == that.topicTemperature) {
+            temperatureService.setCharacteristic(Characteristic.CurrentTemperature, info.temperature);
+        }
+        if (topic == that.topicHumidity) {
+            humiditySensor.setCharacteristic(Characteristic.CurrentRelativeHumidity, info.humidity);
+        }
+	});
+    this.client.subscribe(this.topicStatusGet);
+}
+
+
+
+MqttSwitchAccessory.prototype = {
+    identify: function (callback) {
+        this.log("Identify requested!");
+        callback(); // success
+    },
+
+	getStatus: function(callback) {
+    	callback(null, this.switchStatus);
+	},
+
+	setStatus: function(status, callback, context) {
+		if(context !== 'fromSetValue') {
+			this.switchStatus = status;
+		    this.client.publish(this.topicStatusSet, status ? "true" : "false");
+		}
+		callback();
+	},
+
+	getServices: function() {
+		var informationService = new Service.AccessoryInformation();
+
+		informationService
+		.setCharacteristic(Characteristic.Name, this.name)
+		.setCharacteristic(Characteristic.Manufacturer, this.manufacturer)
+		.setCharacteristic(Characteristic.Model, this.model)
+		.setCharacteristic(Characteristic.SerialNumber, this.serial);
+
+
+        switchService = new Service.Switch(this.name);
+        switchService
+            .getCharacteristic(Characteristic.On)
+            .on('get', this.getStatus.bind(this))
+            .on('set', this.setStatus.bind(this));
+
+        temperatureService = new Service.TemperatureSensor(this.name);
+        temperatureService
+                .getCharacteristic(Characteristic.CurrentTemperature)
+                .on('get', this.getState.bind(this));
+
+        humidityService = new Service.HumiditySensor(this.name);
+        humidityService
+                .getCharacteristic(Characteristic.CurrentRelativeHumidity)
+                .on('get', this.getState.bind(this));
+
+		return [informationService, this.service, temperatureService, humidityService];
+	}
+};
